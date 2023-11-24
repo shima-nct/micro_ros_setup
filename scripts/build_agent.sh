bd2e10e6396e9d03ca7f0fb374dab9cc7787b423
SHIMA Naoki <shima@numazu-ct.ac.jp>
2023-11-24 19:50:11 +0900

    Update custom repository and build option

--- scripts/build_agent.sh
+++ scripts/build_agent.sh
@@ -9,4 +9,5 @@ echo "Building micro-ROS Agent"
 colcon build --packages-up-to micro_ros_agent $@ --cmake-args \
     "-DUAGENT_BUILD_EXECUTABLE=OFF" \
     "-DUAGENT_P2P_PROFILE=OFF" \
-    "--no-warn-unused-cli"
+    "--no-warn-unused-cli" \
+    "-DCMAKE_BUILD_TYPE=Debug"
